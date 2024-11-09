4bc3395d3270a726bf5a7235e40cf0a703937185
Carter Costic <cartercostic@gmail.com>
2024-11-09 14:22:11 -0500

    update

--- smc_test/main.py
+++ smc_test/main.py
@@ -22,18 +22,19 @@ async def main():
     m = len(mpc.parties)
     peers = list(range(m))
 
-    client_data: List[List[Tuple[int, float]]] = [
-        [(1, 0.8), (1, 0.3)],  # Client 1's data
-        [(1, 0.5), (1, 0.0)],  # Client 2's data
-        [(1, 0.8), (1, 0.1)],  # Client 3's data
-        # [(1, 30), (1, 25)],  # Client 3's data
+    client_data: List[List[Tuple[str, float]]] = [
+        [("Cardiologist", 80), ("Orthopedic", 3)],
+        [("Orthopedic", 50), ("Cardiologist", 2)],
+        [("Orthopedic", 80), ("Cardiologist", 10)],
     ]
 
     await mpc.start()
     # dont do list of lists to make sending input easier (maybe change)
     id = lambda idx: secnum(mpc.pid * 100 + idx)
+    to_int = lambda s: int.from_bytes(s.encode(), "big")
+
     myinput = [
-        [id(idx), secnum(it[0]), secnum(int(it[1] * 10))]
+        [id(idx), secnum(to_int(it[0])), secnum(it[1])]
         for idx, it in enumerate(client_data[mpc.pid])
     ]
     inputs = mpc.input(flatten(myinput), senders=peers)
@@ -43,27 +44,18 @@ async def main():
 
     unique_tags = [entry[0] for entry in sorted_union]  # Extract only unique tags
     revealed_tags = await mpc.output(unique_tags)
-    # out = await mpc.output(flatten(sorted_union))
-    # p_out = aggregate(out, 3)
-    # my_positions = [i for i, tag in enumerate(revealed_tags) if tag // 100 == mpc.pid]
 
-    # for idx, it in enumerate(client_data[mpc.pid]):
-    #     pos = revealed_tags.index(id(idx))
-    #     print(it, revealed_tags[pos])
-
-    my_positions = []
+    print("local view")
     for i, tag in enumerate(revealed_tags):
         if tag // 100 == mpc.pid:
             client_idx = tag - (mpc.pid * 100)
             print(i, client_data[mpc.pid][client_idx])
 
-    # Output sorted positions with associated input data for the current party
-    print(f"Party {mpc.pid} sorted positions and associated data: {my_positions}")
-    #
+    # remove global sort in final when move away from static input
     global_sort = sorted(flatten(client_data), key=lambda it: it[1])
+    print("global view")
     for idx, g in enumerate(global_sort):
         print(f"{idx}: {g}")
-    print("global view", global_sort)
 
     await mpc.shutdown()
 
