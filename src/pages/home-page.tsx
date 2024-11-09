18df369e4b02b9a4ce5603b8d549b93468ca4962
kevin-le1 <kl3059@princeton.edu>
2024-11-08 19:58:55 -0500

    shadcn ui button & checkbox for fun

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,8 +1,28 @@
 import '../App.css'
+import {Checkbox} from "../components/ui/checkbox"
+import {Button} from "../components/ui/button"
 
 export default function Home () {
 
   return (
-    <div>Home</div>
+    <>
+        <div>Home</div>
+        
+        <div className="flex space-x-3">
+
+
+        <Checkbox
+        id="terms"
+        className="h-5 w-5 text-blue-500 border-gray-300 rounded focus:ring-blue-500 focus:ring-2 dark:text-blue-500 dark:border-gray-600 dark:focus:ring-blue-500 hover:border-blue-700 hover:bg-blue-100"
+        />
+        <label
+        htmlFor="terms"
+        className="text-sm font-medium text-white dark:text-gray-300 cursor-pointer"
+        >
+        Accept terms and conditions
+        </label>
+        <Button>Start here</Button>
+        </div>
+        </>
   )
 }
